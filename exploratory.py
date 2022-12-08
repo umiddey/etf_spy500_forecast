f32705fd1c782aa08b775405d91e7412cfb55229
KD <umiddey@gmail.com>
2022-12-08 22:35:51 +0100

    backtesting a strategy given by chat gpt

--- exploratory.py
+++ exploratory.py
@@ -1,14 +1,59 @@
+import backtrader as bt
 import pandas as pd
-import plotly.express as px
-import matplotlib.pyplot as plt
+from sklearn.neural_network import MLPClassifier
 
-df = pd.read_csv('Macrotrends-s-p-500-index-daily.csv', skiprows=8)
+# Load the historical data for the S&P 500
+data = pd.read_csv('spy500_data_2021_2022.csv')
 
-work = df[(df['Date'] >= '2020-01-01')].set_index('Date')
-work.index = pd.to_datetime(work.index)
+# Create a subclass of the Strategy class
+class SP500Strategy(bt.Strategy):
+    def __init__(self):
+        # Set the rules for buying and selling stocks
+        self.stop_loss = 0.95 # Stop loss at 5% below the current price
+        self.take_profit = 1.05 # Take profit at 5% above the current price
 
-fig = px.line(data_frame = work )
-fig.write_html('first_figure.html', auto_open=True)
+        # Choose the features to use in the model
+        features = ['open', 'high', 'low', 'close', 'volume']
 
-plt.plot_date(x = work.index, y= work)
-plt.legend()
+        # Use the "close" column as the target variable
+        target = data['close']
+
+        # Train a neural network using the features and target
+        self.model = MLPClassifier(hidden_layer_sizes=(50, 50, 50), max_iter=1000)
+        self.model.fit(data[features], target)
+
+    def next(self):
+        # Use the model to make a prediction for the current time period
+        prediction = self.model.predict([self.datas[0].open[0],
+                                         self.datas[0].high[0],
+                                         self.datas[0].low[0],
+                                         self.datas[0].close[0],
+                                         self.datas[0].volume[0]])
+
+        if prediction == 'buy' and self.datas[0].close[0] < self.stop_loss:
+            # Buy the stock at the current price
+            self.buy()
+        elif prediction == 'sell' and self.datas[0].close[0] > self.take_profit:
+            # Sell the stock at
+            self.sell()
+
+    # Create an instance of the backtrader.Strategy class
+strategy = SP500Strategy()
+
+# Create an instance of the backtrader.cerebro.Cerebro class
+cerebro = bt.Cerebro()
+
+# Add the strategy to cerebro
+cerebro.addstrategy(strategy)
+
+# Create a Data Feed for the S&P 500 data
+data = bt.feeds.PandasData(dataname=data)
+
+# Add the data feed to cerebro
+cerebro.adddata(data)
+
+# Run the backtest
+cerebro.run()
+
+# Print the final portfolio value
+print('Final Portfolio Value:', cerebro.broker.getvalue())
